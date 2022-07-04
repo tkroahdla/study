8045c4d96c815a9c34a6f04439691921a35880fb
jin <tkroahdla@naver.com>
2022-07-04 20:58:16 +0900

    0704

--- React/myapp/src/App.js
+++ React/myapp/src/App.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, satValue] = useState(0);
   const onClick = () => satValue((prev) => prev + 1);
-  console.log("call an api");
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
