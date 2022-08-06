c4923467d33b507c091afc0335612eb8c4660562
제동진 <43737240+tkroahdla@users.noreply.github.com>
2022-08-07 03:54:15 +0900

    Form

--- masterclassts/src/App.tsx
+++ masterclassts/src/App.tsx
@@ -1,10 +1,27 @@
-import Circle from './Circle';
+import React, { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState('');
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const asdf = e.currentTarget.value;
+    setValue(asdf);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('hello', value);
+  };
+
   return (
     <div>
-      <Circle borderColor="yellow" bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
