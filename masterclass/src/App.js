7867a659297a3f95ffce685d005fbc5a4d98b53e
제동진 <43737240+tkroahdla@users.noreply.github.com>
2022-08-06 16:01:23 +0900

    theme

--- masterclass/src/App.js
+++ masterclass/src/App.js
@@ -1,41 +1,22 @@
 import styled, { keyframes } from 'styled-components';
 
-// const Box = styled.div`
-//   background-color: ${(props) => props.bgColor};
-//   width: 100px;
-//   height: 100px;
-// `;
-
-// // Box의 모든 속성들을 들고 온 다음 추가적으로 아래 코드를 더해준다.
-// const Circle = styled(Box)`
-//   border-radius: 50px;
-// `;
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
-  width: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-`;
-
-const anim = keyframes`
-from{
-  color:tomato;
-}
-to{
-  color:teal;
-}
-`;
-
-const Btn = styled.button`
-  animation: ${anim} 0.5s infinite;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Btn>asdf</Btn>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
--- masterclass/src/index.js
+++ masterclass/src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProps, ThemeProvider } from 'styled-components';
+
+const darkTheme = {
+  textColor: 'whitesmoke',
+  backgroundColor: '#111',
+};
+
+const lightTheme = {
+  textColor: '#111',
+  backgroundColor: 'whitesmoke',
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
