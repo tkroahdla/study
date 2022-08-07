480875c4109e76e7bdc776c7ebf86c608c37dc11
제동진 <43737240+tkroahdla@users.noreply.github.com>
2022-08-08 02:38:54 +0900

    Delete : ggql

--- ggql/server.js
+++
@@ -1,9 +0,0 @@
-import { ApolloServer, gql } from 'apollo-server-express';
-
-const server = new ApolloServer({});
-
-server.listen().then(({ url }) => {
-  console.log(`Runing on ${url}`);
-});
-
-
