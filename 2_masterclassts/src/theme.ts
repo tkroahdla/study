69e5a40a0bd3ea4df2f824152d32542988b119e2
제동진 <43737240+tkroahdla@users.noreply.github.com>
2022-08-09 00:02:07 +0900

    Coin Components

--- 2_masterclassts/src/theme.ts
+++ 2_masterclassts/src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from 'styled-components';
 
 export const theme: DefaultTheme = {
-  bgColor: 'white',
-  textColor: 'black',
-  btnColor: 'tomato',
+  bgColor: `#2980b9`,
+  textColor: '#e67e22',
+  accentColor: '#e74c3c',
 };
