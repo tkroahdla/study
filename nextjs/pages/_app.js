b3ee5d27a89dede13968b3b95f4f241e4e48b01a
제동진 <43737240+tkroahdla@users.noreply.github.com>
2022-09-01 01:32:59 +0900

    _app.js

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import NavBar from '../components/NavBar';
+import '../styles/globals.css';
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        a {
+          color: white;
+        }
+      `}</style>
+    </>
+  );
 }
-
-export default MyApp
---
+++ nextjs/pages/about.js
@@ -0,0 +1,9 @@
+import NavBar from '../components/NavBar';
+
+export default function about() {
+  return (
+    <div>
+      <h1>about</h1>
+    </div>
+  );
+}
