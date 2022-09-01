e2b248e8b2f005471b1b5427316543a5f7091a32
제동진 <43737240+tkroahdla@users.noreply.github.com>
2022-09-02 02:06:37 +0900

    api

--- nextjs/pages/about.js
+++ nextjs/pages/about.js
@@ -1,8 +1,11 @@
+import Head from 'next/head';
 import NavBar from '../components/NavBar';
+import Seo from '../components/Seo';
 
 export default function about() {
   return (
     <div>
+      <Seo title="About" />
       <h1>about</h1>
     </div>
   );
