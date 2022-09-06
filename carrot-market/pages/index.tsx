a1203ec43621ead12008481fa748d09b94d36ea0
제동진 <43737240+tkroahdla@users.noreply.github.com>
2022-09-06 15:42:05 +0900

    #4.2 Test Drive part Two

---
+++ carrot-market/pages/index.tsx
@@ -0,0 +1,57 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-blue-100 py-20 px-20 grid gap-5">
+      <div className="bg-white p-6 rounded-3xl shadow-xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$10</span>
+        </div>
+        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto">
+          Checkout
+        </div>
+      </div>
+
+      <div className="bg-white overflow-hidden rounded-2xl shadow-xl">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="rounded-3xl p-6 bg-white relative -top-5">
+          <div className="flex relative -top-16 items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 bg-red-400 rounded-full"></div>
+
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$2,310</span>
+            </div>
+          </div>
+
+          <div className="relative mt-15 flex flex-col items-center -mt-10 -mb-5">
+            <span className="text-lg font-medium">Tony Molloy</span>
+            <span className="text-sm text-gray-500">미국</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ carrot-market/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx,tx,tsx}',
+    './components/**/*.{js,jsx,tx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
