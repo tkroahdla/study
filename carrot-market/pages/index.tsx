fb67c12001c4387c51f29721c61101adb512cead
제동진 <43737240+tkroahdla@users.noreply.github.com>
2022-09-07 01:48:12 +0900

    #4.13 Conclusions

--- carrot-market/pages/index.tsx
+++ carrot-market/pages/index.tsx
@@ -2,25 +2,9 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <form className="flex flex-col space-y-2 p-5 ">
-      <input
-        type="text"
-        required
-        placeholder="Username"
-        className="peer rounded-full border border-gray-400 p-1"
-      />
-      <span className="hidden peer-invalid:block peer-invalid:text-red-500">
-        This input is invalid
-      </span>
-      <span className="hidden peer-valid:block peer-valid:text-teal-500">
-        Awesome username
-      </span>
-      <span className="hidden peer-invalid:text-yellow-500 peer-hover:block">
-        Hello
-      </span>
-
-      <input type="submit" value="Login" className="bg-white" />
-    </form>
+    <div className="dark:md:hover:bg-teal-400">
+      <h2 className="text-[3322px]">asdf</h2>
+    </div>
   );
 };
 
