5a2e5eca1b15a27d5ceee8ae63bd14ce0d97a2f8
DJ <tkroahdla@naver.com>
2022-12-15 02:48:14 +0900

    START RN #2.1

---
+++ ReactNative/NomadWeather/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hellofff</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 28,
+    color: "black",
+  },
+});
