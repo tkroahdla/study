c37aaa8a5f43fe52ead0ad074ebd84122f26c139
DJ <tkroahdla@naver.com>
2022-12-30 02:23:45 +0900

    #2.8

--- ReactNative/NomadWeather/App.js
+++ ReactNative/NomadWeather/App.js
@@ -1,24 +1,105 @@
+import {
+  ActivityIndicator,
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions } from "react-native";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+const API_KEY = "";
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`
+    );
+    const json = await response.json();
+    console.log(json);
+    setDays(json.daily);
+  };
+  useEffect(() => {
+    getWeather();
+  });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hellofff</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="black" />
+      <View style={styles.containter}>
+        <View style={styles.city}>
+          <Text style={styles.cityName}>{city}</Text>
+        </View>
+        <ScrollView
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          contentContainerStyle={styles.weather}
+        >
+          {days?.length == 0 ? (
+            <View style={styles.day}>
+              <ActivityIndicator size="large" color="#00ff00" />
+              <Text style={styles.loading}>loading...</Text>
+            </View>
+          ) : (
+            <View style={styles.day}></View>
+          )}
+        </ScrollView>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containter: {
     flex: 1,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "teal",
   },
-  text: {
-    fontSize: 28,
-    color: "black",
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  desc: {
+    marginTop: -30,
+    fontSize: 60,
+  },
+  loading: {
+    marginTop: 30,
+    fontSize: 30,
   },
 });
