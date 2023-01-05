bfe7a515bacc76cc214a31bc5ed0b1aa7a76f9f7
DJ <tkroahdla@naver.com>
2023-01-06 00:48:51 +0900

    #2.10

--- ReactNative/NomadWeather/App.js
+++ ReactNative/NomadWeather/App.js
@@ -1,18 +1,23 @@
-import {
-  ActivityIndicator,
-  Text,
-  View,
-  StyleSheet,
-  ScrollView,
-} from "react-native";
+import { ActivityIndicator, Text, View, StyleSheet, ScrollView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Dimensions } from "react-native";
 import { useEffect, useState } from "react";
 import * as Location from "expo-location";
+import { Fontisto } from "@expo/vector-icons";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 const API_KEY = "";
+
+const icons = {
+  Clouds: "cloudy",
+  Rain: "rains",
+  Clear: "day-sunny",
+  Atmosphere: "",
+  Snow: "snow",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
 
 export default function App() {
   const [city, setCity] = useState("Loading...");
@@ -27,15 +32,10 @@ export default function App() {
       coords: { latitude, longitude },
     } = await Location.getCurrentPositionAsync({ accuracy: 5 });
 
-    const location = await Location.reverseGeocodeAsync(
-      { latitude, longitude },
-      { useGoogleMaps: false }
-    );
+    const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMaps: false });
     setCity(location[0].city);
-
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
-    );
+    console.log(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
     const json = await response.json();
     console.log(json);
     setDays(json.daily);
@@ -55,12 +55,7 @@ export default function App() {
         <View style={styles.city}>
           <Text style={styles.cityName}>{city}</Text>
         </View>
-        <ScrollView
-          pagingEnabled
-          showsHorizontalScrollIndicator={false}
-          horizontal
-          contentContainerStyle={styles.weather}
-        >
+        <ScrollView pagingEnabled showsHorizontalScrollIndicator={false} horizontal contentContainerStyle={styles.weather}>
           {days?.length == 0 ? (
             <View style={styles.day}>
               <ActivityIndicator size="large" color="#00ff00" />
@@ -69,13 +64,13 @@ export default function App() {
           ) : (
             days.map((day, index) => (
               <View key={index} style={styles.day}>
-                <Text style={styles.temp}>
-                  {parseFloat(day.temp.day).toFixed(1)}
-                </Text>
+                <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "100%" }}>
+                  <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                  <Fontisto name={icons[day.weather[0].main]} size={68} color="white" />
+                </View>
+
                 <Text style={styles.desc}>{day.weather[0].main}</Text>
-                <Text style={styles.tinyText}>
-                  {day.weather[0].description}
-                </Text>
+                <Text style={styles.tinyText}>{day.weather[0].description}</Text>
               </View>
             ))
           )}
@@ -88,29 +83,34 @@ export default function App() {
 const styles = StyleSheet.create({
   containter: {
     flex: 1,
-    backgroundColor: "teal",
+    backgroundColor: "tomato",
   },
   city: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    marginTop: 40,
   },
   cityName: {
     fontSize: 68,
     fontWeight: "500",
+    color: "white",
   },
   weather: {},
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: "flex-start",
+    paddingHorizontal: 20,
   },
   temp: {
     marginTop: 50,
-    fontSize: 178,
+    fontSize: 120,
+    color: "white",
   },
   desc: {
     marginTop: -30,
-    fontSize: 60,
+    fontSize: 30,
+    color: "white",
   },
   loading: {
     marginTop: 30,
@@ -118,5 +118,6 @@ const styles = StyleSheet.create({
   },
   tinyText: {
     fontSize: 20,
+    color: "white",
   },
 });
