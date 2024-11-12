9e015131b33a8b42a8520caed4d2141e6133b909
DJ <tkroahdla@naver.com>
2024-11-12 22:11:32 +0900

    nextjs14 - #2.8

---
+++ nextjs14/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: 'Home',
+}
+
+export default function AboutUs() {
+    return (
+        <div>
+            <h1>Hello!</h1>
+        </div>
+    )
+}
--- nextjs14/app/page.tsx
+++ nextjs14/app/page.tsx
@@ -1,8 +1,7 @@
-import Navigation from "../components/navigation";
+
 
 export default function Tomato() {
     return <div>
-        <Navigation />
         <h1>Hello!</h1>
     </div>
 }
