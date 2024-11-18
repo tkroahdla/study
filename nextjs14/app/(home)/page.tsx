4ab2bcf138a7d76bb6bc1fee3e363977b52b8e66
DJ <tkroahdla@naver.com>
2024-11-18 23:53:44 +0900

    #3.2 Server Side

--- nextjs14/app/(home)/page.tsx
+++ nextjs14/app/(home)/page.tsx
@@ -1,11 +1,18 @@
 export const metadata = {
-    title: 'Home',
+    title: "Home",
 }
 
-export default function AboutUs() {
-    return (
-        <div>
-            <h1>Hello!</h1>
-        </div>
-    )
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
+
+async function getMovies() {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log("im fetching!!!")
+    const response = await fetch(URL);
+    const json = await response.json();
+    return json;
+}
+
+export default async function HomePage() {
+    const movies = await getMovies();
+    return <div>{JSON.stringify(movies)}</div>
 }
--- nextjs14/app/about-us/company/jobs/sales/page.tsx
+++ nextjs14/app/about-us/company/jobs/sales/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-    return <h1>Sales jobs!</h1>
+    return <div><h1>Sales jobs!</h1></div>
 }
