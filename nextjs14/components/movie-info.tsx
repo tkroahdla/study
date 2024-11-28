2e94a77e7dab4230ff92e429b8d47610df351e95
jin <tkroahdla@naver.com>
2024-11-28 12:47:25 +0900

    #4.2 Movie Styles

--- nextjs14/components/movie-info.tsx
+++ nextjs14/components/movie-info.tsx
@@ -1,17 +1,11 @@
 import { API_URL } from "../app/(home)/page";
 
 async function getMovie(id: string) {
-    console.log(`Fethiing movies : ${Date.now()}`)
-    // await new Promise((resolve) => setTimeout(resolve, 5000));
     const response = await fetch(`${API_URL}/${id}`);
     return response.json();
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
     const movie = await getMovie(id);
-    return (
-        <div>
-            <h6>{JSON.stringify(movie)}</h6>
-        </div>
-    )
+    return <h6>{JSON.stringify(movie)}</h6>;
 }
--- nextjs14/components/movie-videos.tsx
+++ nextjs14/components/movie-videos.tsx
@@ -1,18 +1,11 @@
 import { API_URL } from "../app/(home)/page";
 
-async function getVidoes(id: string) {
-    console.log(`Fethiing videos : ${Date.now()}`)
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-    throw new Error('something broke...')
-    // const response = await fetch(`${API_URL}/${id}/videos`);
-    // return response.json();
+async function getVideos(id: string) {
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
-    const videos = await getVidoes(id);
-    return (
-        <div>
-            <h6>{JSON.stringify(videos)}</h6>
-        </div>
-    )
+    const videos = await getVideos(id);
+    return <h6>{JSON.stringify(videos)}</h6>;
 }
